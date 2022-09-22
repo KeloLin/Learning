d4895e2690b5e4a70f1aba66cede01df1070d005
KeloLin <kelolin@vip.qq.com>
2022-09-22 22:31:12 +0800

    新增NIO

--- src/main/java/com/learn/review/netty/NettyServer.java
+++ src/main/java/com/learn/review/netty/NettyServer.java
@@ -1,5 +1,9 @@
 package com.learn.review.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @Author: Kelo
  * @Date: 2022/9/21
@@ -7,6 +11,15 @@ package com.learn.review.netty;
 public class NettyServer {
 
     public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        bootstrap.group(bossGroup, workerGroup)
+        .channel(NioServerSocketChannel.class)
+        .option()
+        ;
 
     }
 }
